2331938f00a60edc87a74fe311056747cdaa14cc
Alexandre <alexmcs@gmail.com>
2023-01-18 09:49:59 -0300

    Implementando a função de carrinho de compras

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Product from './components/Product';
 import Footer from './components/Footer';
 
+const products = [
+  {
+    image: '/images/tenis-azul.jpg',
+    title: 'Tênis Asics Gel-rocket 10 Masculino',
+    description: 'Ideal para jogar vôlei, o Tênis Asics Gel-Rocket 10 Masculino oferece tração elevada e conta com reforço na biqueira para maior firmeza e segurança nos lances dentro da quadra, assim como a tecnologia Trusstic System, que fornece maior estabilidade torsional.',
+    price: '499,00'
+  },
+  {
+    image: '',
+    title: '',
+    description: '',
+    price: ''
+  },
+  {
+    image: '',
+    title: '',
+    description: '',
+    price: ''
+  },
+  {
+    image: '',
+    title: '',
+    description: '',
+    price: ''
+  }  
+]
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Product image='/images/tenis-azul.jpg' title='Tênis Asics Gel-rocket 10 Masculino' description=' Ideal para jogar vôlei, o Tênis Asics Gel-Rocket 10 Masculino oferece tração elevada e conta com reforço na biqueira para maior firmeza e segurança nos lances dentro da quadra, assim como a tecnologia Trusstic System, que fornece maior estabilidade torsional. O tênis masculino de vôlei possui cabedal produzido em material sintético para maior estabilidade e recortes em tecido para maior respirabilidade. Com tecnologia Asics a favor da sua performance, você tem todo o suporte e amortecimento Gel para dominar os treinos e competições de vôlei. Já o solado traz borracha resistente com ponto de giro para facilitar o seu movimento dentro do jogo.' price='499,00' />
+      {products.map(product =>
+        <Product
+          key={product.title}
+          title={product.title}
+          image={product.image}
+          description={product.description}
+          price={product.price}
+        />)}
       <Footer />
     </div>
   );
--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ProductContainer = styled.div`
-    width: 200px;
+    width: 300px;
   border: 1px solid #ccc;
   padding: 20px;
   margin-bottom: 20px;
@@ -13,8 +13,9 @@ const ProductImage = styled.img`
 `;
 
 const ProductTitle = styled.h3`
-  margin-top: 0;
+  margin-top: 30px;
   margin-bottom: 10px;
+  font-size: 20px;
 `;
 
 const ProductDescription = styled.p`
@@ -24,9 +25,20 @@ const ProductDescription = styled.p`
 `;
 
 const ProductPrice = styled.p`
-  font-size: 18px;
+  font-size: 22px;
   font-weight: bold;
   color: #333;
+`;
+
+const AddToCartButton = styled.button`
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  &:hover {
+    background-color: #555;
+  }
 `;
 
 function Product(props) {
@@ -36,6 +48,7 @@ function Product(props) {
             <ProductTitle>{props.title}</ProductTitle>
             <ProductDescription>{props.description}</ProductDescription>
             <ProductPrice>R${props.price}</ProductPrice>
+            <AddToCartButton onClick={() => props.addToCart(props.productId)}>Adicionar ao carrinho</AddToCartButton>
         </ProductContainer>
     );
 }
