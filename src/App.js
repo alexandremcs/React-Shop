202ea736b36e9f53da6b694b9f6f01df5e816695
Alexandre <alexmcs@gmail.com>
2023-01-21 15:46:40 -0300

    corrigindo estilo do app.js

--- src/App.js
+++ src/App.js
@@ -13,22 +13,34 @@ const products = [
     price: '499,00'
   },
   {
-    image: '',
-    title: '',
-    description: '',
-    price: ''
+    image: '/images/tenis-azul.jpg',
+    title: 'Tênis Asics Gel-rocket 10 Masculino',
+    description: 'Ideal para jogar vôlei, o Tênis Asics Gel-Rocket 10 Masculino oferece tração elevada e conta com reforço na biqueira para maior firmeza e segurança nos lances dentro da quadra, assim como a tecnologia Trusstic System, que fornece maior estabilidade torsional.',
+    price: '499,00'
   },
   {
-    image: '',
-    title: '',
-    description: '',
-    price: ''
+    image: '/images/tenis-azul.jpg',
+    title: 'Tênis Asics Gel-rocket 10 Masculino',
+    description: 'Ideal para jogar vôlei, o Tênis Asics Gel-Rocket 10 Masculino oferece tração elevada e conta com reforço na biqueira para maior firmeza e segurança nos lances dentro da quadra, assim como a tecnologia Trusstic System, que fornece maior estabilidade torsional.',
+    price: '499,00'
   },
   {
-    image: '',
-    title: '',
-    description: '',
-    price: ''
+    image: '/images/tenis-azul.jpg',
+    title: 'Tênis Asics Gel-rocket 10 Masculino',
+    description: 'Ideal para jogar vôlei, o Tênis Asics Gel-Rocket 10 Masculino oferece tração elevada e conta com reforço na biqueira para maior firmeza e segurança nos lances dentro da quadra, assim como a tecnologia Trusstic System, que fornece maior estabilidade torsional.',
+    price: '499,00'
+  },
+  {
+    image: '/images/tenis-azul.jpg',
+    title: 'Tênis Asics Gel-rocket 10 Masculino',
+    description: 'Ideal para jogar vôlei, o Tênis Asics Gel-Rocket 10 Masculino oferece tração elevada e conta com reforço na biqueira para maior firmeza e segurança nos lances dentro da quadra, assim como a tecnologia Trusstic System, que fornece maior estabilidade torsional.',
+    price: '499,00'
+  },
+  {
+    image: '/images/tenis-azul.jpg',
+    title: 'Tênis Asics Gel-rocket 10 Masculino',
+    description: 'Ideal para jogar vôlei, o Tênis Asics Gel-Rocket 10 Masculino oferece tração elevada e conta com reforço na biqueira para maior firmeza e segurança nos lances dentro da quadra, assim como a tecnologia Trusstic System, que fornece maior estabilidade torsional.',
+    price: '499,00'
   }
 ]
 
@@ -37,14 +49,16 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <Header />
-        {products.map(product =>
-          <Product
-            key={product.title}
-            title={product.title}
-            image={product.image}
-            description={product.description}
-            price={product.price}
-          />)}
+        <div className='main'>
+          {products.map(product =>
+            <Product
+              key={product.title}
+              title={product.title}
+              image={product.image}
+              description={product.description}
+              price={product.price}
+            />)}
+        </div>
         <Footer />
       </div>
     </Provider>
--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ProductContainer = styled.div`
-    width: 300px;
+  width: 300px;
   border: 1px solid #ccc;
   padding: 20px;
   margin-bottom: 20px;
